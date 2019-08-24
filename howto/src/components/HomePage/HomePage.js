8914ad25fe640cc66c78a30b399eca41e342fa96
Chineek Favors <chineekfavors01@gmail.com>
2019-08-23 18:35:14 -0700

    added form for create,edit,search

--- howto/src/components/HomePage/HomePage.js
+++ howto/src/components/HomePage/HomePage.js
@@ -2,16 +2,25 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-	    <div>
+	    <div className="homepage">
 	    	<header>
-	    	<h1>Home Page</h1>
+	    		<h1>Home Page</h1>
 	    	</header>
 	    	<h3>Hello "userName"</h3>
-	    	<label htmlFor="search">Search for a How-To-Guide:</label>
-	    	<input type="search" id="site-search" name="search" />
-	     	<button>Search</button>
-	      	<section className="homePage">
-	       the section area
+
+	    	<form className="formHomePage">
+		    	<label htmlFor="create">Create a new How-To-Guide:</label>
+		    	<button className="createButton" name="create">Create</button>
+
+		    	<label htmlFor="edit">Edit a existing How-To-Guide:</label>
+		    	<button className="editButton" name="edit">edit</button>
+
+		    	<label htmlFor="search">Search for a How-To-Guide:</label>
+		    	<input type="search" id="searchHomePage" name="search" />
+		     	<button className="searchButton" name="search">Search</button>
+	     	</form>
+	      	<section className="sectionHomePage">
+	       the section area for rendering
 	      </section>
 	    </div>
   );
