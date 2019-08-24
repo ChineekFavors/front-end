c9d5171c9f5510fcc66cfb93bef84081f22546fa
Chineek Favors <chineekfavors01@gmail.com>
2019-08-23 18:10:01 -0700

    added header

--- howto/src/App.js
+++ howto/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 //components
 import LandingPage from './components/LandingPage/LandingPage';
+import HomePage from './components/HomePage/HomePage.js';
 
 function App() {
   return (
     <div className='App'>
       <LandingPage />
+      <HomePage />
     </div>
   );
 }
--- howto/src/components/HomePage/HomePage.js
+++ howto/src/components/HomePage/HomePage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function HomePage(){
-
-	return(
-		<>
-		</>
-	)
-}
+const HomePage = () => {
+  return (
+	    <div>
+	    	<header>
+	    	<h1>Home Page</h1>
+	    	</header>
+	    	<h3>Hello "userName"</h3>
+	    	<label htmlFor="search">Search for a How-To-Guide:</label>
+	    	<input type="search" id="site-search" name="search" />
+	     	<button>Search</button>
+	      	<section className="homePage">
+	       the section area
+	      </section>
+	    </div>
+  );
+};
 
 export default HomePage;
